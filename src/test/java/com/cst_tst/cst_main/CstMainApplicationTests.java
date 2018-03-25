a91dd9d8af1f8b2e3cf7cad612844fb79cfbf330
Rudy <rudy.carrillo1989@gmail.com>
2018-03-25 12:16:44 -0600

    first commit with web libs

--- src/test/java/com/cst_tst/cst_main/CstMainApplicationTests.java
+++ src/test/java/com/cst_tst/cst_main/CstMainApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cst_tst.mainback.cst_back;
+package com.cst_tst.cst_main;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class CstMainApplicationTests {
 
 	@Test
 	public void contextLoads() {
