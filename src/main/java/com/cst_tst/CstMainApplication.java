134060f9dd571ca64f703a9bac3ed68d9b8f2c2a
Rudy <rudy.carrillo1989@gmail.com>
2018-03-25 14:33:57 -0600

    initiated spring: Hello world!!"

--- src/main/java/com/cst_tst/CstMainApplication.java
+++ src/main/java/com/cst_tst/CstMainApplication.java
@@ -1,4 +1,4 @@
-package com.cst_tst.cst_main;
+package com.cst_tst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,5 +8,6 @@ public class CstMainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CstMainApplication.class, args);
+		System.out.println("Started");
 	}
 }
