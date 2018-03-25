e4e39822aa9073aecae3f7d21df0bef31a9775eb
Rudy <rudy.carrillo1989@gmail.com>
2018-03-25 16:12:33 -0600

    next commit

--- src/main/java/com/cst_tst/dao/CST_UserRepository.java
+++ src/main/java/com/cst_tst/dao/CST_UserRepository.java
@@ -1,8 +1,6 @@
 package com.cst_tst.dao;
 
-
 import com.cst_tst.model.CST_UserModel;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface CST_UserRepository extends CrudRepository<CST_UserModel, Long>{
--- src/main/java/com/cst_tst/model/CST_UserModel.java
+++ src/main/java/com/cst_tst/model/CST_UserModel.java
@@ -44,4 +44,79 @@ public class CST_UserModel {
     
     @Column(name="date_modified", nullable=false)
     public Date date_modified;
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getPhone1() {
+		return phone1;
+	}
+
+	public void setPhone1(String phone1) {
+		this.phone1 = phone1;
+	}
+
+	public Date getDate_added() {
+		return date_added;
+	}
+
+	public void setDate_added(Date date_added) {
+		this.date_added = date_added;
+	}
+
+	public Date getDate_modified() {
+		return date_modified;
+	}
+
+	public void setDate_modified(Date date_modified) {
+		this.date_modified = date_modified;
+	}
+
+
+    
 }
